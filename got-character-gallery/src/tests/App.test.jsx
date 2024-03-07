8d00c6785f690ddf3a7e4cf6509252a4c2cc6fa5
Victoria Adeyemi <151677799+victoriadeyemi@users.noreply.github.com>
2024-03-07 19:58:54 +0000

    removed vite src

--- got-character-gallery/src/tests/App.test.jsx
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import App from '../App';
-
-it("renders the app", () => {
-    render(<App />);
-});
